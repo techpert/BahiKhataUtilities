83b2d59b9020b4c9dd53cca03d2246bf2c9cb3ea
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2019-07-28 19:43:17 +0530

    Removed autoconfigure security

---
+++ src/main/java/bahikhata/utilities/jwtutilities/BahiKhataJwtConverter.java
@@ -0,0 +1,21 @@
+package bahikhata.utilities.jwtutilities;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author Rudhra.Koul
+ *
+ */
+@Component
+public class BahiKhataJwtConverter extends DefaultAccessTokenConverter implements JwtAccessTokenConverterConfigurer{
+
+	@Override
+	public void configure(JwtAccessTokenConverter converter) {
+		converter.setAccessTokenConverter(this);
+	}
+
+}
